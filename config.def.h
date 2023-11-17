1971cf39d1e24dd478749912ed48303a1f8aad49
Steven Wolf <jlwolfi@gmail.com>
2023-11-17 13:37:28 +0100

    symbols

--- config.def.h
+++ config.def.h
@@ -32,7 +32,8 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { " ", " ", " ", " ", "󱞁 ", "󰲫 ", "󰲭 ", "󰲯 ", "󰲱 " };
+//  { " ", " ", " ", " ", "󱞁 ", "󰲫 ", "󰲭 ", "󰲯 ", "󰲱 " }
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -122,6 +123,8 @@ ResourcePref resources[] = {
 };
 
 #include "shiftview.c"
+
+ 
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
