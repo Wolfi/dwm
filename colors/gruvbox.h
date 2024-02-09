838776c47a8e6f4bb9d980bc9dcc3a269ac541c8
Steven Wolf <steven.wolf+git@pm.me>
2024-02-09 12:48:26 +0100

    gruvbox theme

---
+++ colors/gruvbox.h
@@ -0,0 +1,16 @@
+static const char bg0[]      = "#282828";
+static const char bg1[]      = "#3c3836";
+static const char bg2[]      = "#504945";
+
+static const char fg0[]      = "#ebdbb2";
+static const char fg1[]      = "#bdae93";
+static const char fg2[]      = "#a89984";
+
+static const char red[]      = "#cc241d";
+static const char orange[]   = "#d65d0e";
+static const char yellow[]   = "#d79921";
+static const char green[]    = "#98971a";
+static const char bluu[]     = "#458588";
+static const char purple[]   = "#b16286";
+static const char aqua[]     = "#689d6a";
+
