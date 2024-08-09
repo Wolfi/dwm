55e572694a6b544e0b6b3d4860f7655d3130bd92
Steven Wolf <steven.wolf+git@pm.me>
2024-08-09 16:22:08 +0200

    upd

--- colors/mellifluous.h
+++ colors/mellifluous.h
@@ -6,12 +6,12 @@ static const char fg0[]      = "#dadada";
 static const char fg1[]      = "#bfad9e";
 static const char fg2[]      = "#c0af8c";
 
-static const char red[]      = "#cc241d";
-static const char orange[]   = "#d65d0e";
-static const char yellow[]   = "#d79921";
-static const char green[]    = "#98971a";
-static const char bluu[]     = "#458588";
-static const char purple[]   = "#b16286";
-static const char aqua[]     = "#689d6a";
-
+// These are gruvbox-material colors
+static const char red[]      = "#ea6962";
+static const char orange[]   = "#e78a4e";
+static const char yellow[]   = "#d8a657";
+static const char green[]    = "#a9b665";
+static const char aqua[]     = "#89b482";
+static const char bluu[]     = "#7daea3";
+static const char purple[]   = "#d3869b";
 
