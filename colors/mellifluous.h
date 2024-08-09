8f84bf0da32f7149c946b5be9101a585de8a7ce4
Steven Wolf <steven.wolf+git@pm.me>
2024-08-09 15:50:34 +0200

    colors

---
+++ colors/mellifluous.h
@@ -0,0 +1,17 @@
+static const char bg0[]      = "#1a1a1a";
+static const char bg1[]      = "#2d2d2d";
+static const char bg2[]      = "#404040";
+
+static const char fg0[]      = "#dadada";
+static const char fg1[]      = "#bfad9e";
+static const char fg2[]      = "#c0af8c";
+
+static const char red[]      = "#cc241d";
+static const char orange[]   = "#d65d0e";
+static const char yellow[]   = "#d79921";
+static const char green[]    = "#98971a";
+static const char bluu[]     = "#458588";
+static const char purple[]   = "#b16286";
+static const char aqua[]     = "#689d6a";
+
+
